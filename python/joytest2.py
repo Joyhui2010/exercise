414c69429f7aa302c2df4fcfa866a02f61e8a055
yanjiao <yanjiao.hui@hotmail.com>
2024-07-11 23:57:11 +0800

    finish paixu
    Signed-off-by: yanjiao <yanjiao.hui@hotmail.com>

--- python/joytest2.py
+++ python/joytest2.py
@@ -1,5 +1,18 @@
 """
 题目：有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
 """
+def paixu():
+    sum=0
+    for i in [1,2,3,4]:
+        for j in [1,2,3,4]:
+            for k in [1,2,3,4]:
+                if j!=i and k!=i and k!=j:
+                    print(f'{i}{j}{k}')
+                    sum+=1
+                    # print(i,j,k)
+    print(sum)
+paixu()
+
+
 
 
