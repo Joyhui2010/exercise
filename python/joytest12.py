31bac2050839aab515a5de30ae37a418024b05bc
yanjiao <yanjiao.hui@hotmail.com>
2024-07-13 20:22:16 +0800

    add 9-12 answer
    Signed-off-by: yanjiao <yanjiao.hui@hotmail.com>

--- python/joytest12.py
+++ python/joytest12.py
@@ -2,3 +2,14 @@
 题目：两个乒乓球队进行比赛，各出三人。甲队为a,b,c三人，乙队为x,y,z三人。已抽签决定比赛名单。
 有人向队员打听比赛的名单。a说他不和x比，c说他不和x,z比，请编程序找出三队赛手的名单。
 """
+def battal():
+    L1 = ['x', 'y', 'z']
+    for a in L1:
+        for b in L1:
+            if a != b :
+                for c in L1:
+                    if a != c and b != c:
+                        if a != 'x' and c != 'x' and c != 'z':
+                            print('a vs %s\nb vs %s\nc vs %s' % (a, b, c))
+
+battal()
--- python/joytest9.py
+++ python/joytest9.py
@@ -1,3 +1,26 @@
 """
 题目：输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
 """
+
+def count(input_string):
+    letters_count = 0
+    spaces_count = 0
+    digits_count = 0
+    others_count = 0
+    for char in input_string:
+        if char.isalpha():
+            letters_count += 1
+        elif char.isspace():
+            spaces_count += 1
+        elif char.isdigit():
+            digits_count += 1
+        else:
+            others_count += 1
+    return letters_count, spaces_count, digits_count, others_count
+
+input_string = input("请输入一行字符：")
+letters, spaces, digits, others = count(input_string)
+print("英文字母个数:", letters)
+print("空格个数:", spaces)
+print("数字个数:", digits)
+print("其他字符个数:", others)
