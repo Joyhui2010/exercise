050fef59acc9cb581cf5fffbd0ef8492d0d55c24
yanjiao <yanjiao.hui@hotmail.com>
2024-07-13 10:25:53 +0800

    add answer
    Signed-off-by: yanjiao <yanjiao.hui@hotmail.com>

--- python/joytest3.py
+++ python/joytest3.py
@@ -1,3 +1,18 @@
 """
 题目：打印斐波那契数列前10个数
 """
+def rabbit():
+    yi=1
+    er=1
+    n=8
+    print(yi)
+    print(er)
+    while n>0:
+        san=yi+er
+        print(san)
+        yi=er
+        er=san
+        n-=1
+rabbit()
+
+# 1 1 2 3 5 8
